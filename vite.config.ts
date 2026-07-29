2c01281d45451191a5d9f933131af60e86e74dda
Ben ODonnell <ben@green-ark.se>
2026-07-29 22:05:15 +0200

    Fix production build for public site and LIV app

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
+import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   base: '/viktors-liv/',
+  build: {
+    rollupOptions: {
+      input: {
+        website: resolve(__dirname, 'index.html'),
+        app: resolve(__dirname, 'app.html'),
+      },
+    },
+  },
 });
